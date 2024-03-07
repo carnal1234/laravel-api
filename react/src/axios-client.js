e0c4f7e91154f38711b5589ebad5c61aa91a3169
BrainStommy <hahahaxd281@gmail.com>
2024-03-07 20:33:29 +0800

    Remove backend welcome page

--- react/src/axios-client.js
+++ react/src/axios-client.js
@@ -9,7 +9,8 @@ const axiosClient = axios.create({
 axiosClient.interceptors.request.use((config) => {
   const token = localStorage.getItem('ACCESS_TOKEN');
   config.headers = {...config.headers, 
-    "Authorization" :`Bearer ${token}`, "Access-Control-Allow-Origin" : "*"}
+    "Authorization" :`Bearer ${token}`
+  }
 
   return config;
 })
